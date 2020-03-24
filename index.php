82315cabbbd87040924c1a0bcddc1be349f8c042
GinoKielenstijn <55694791+GinoKielenstijn@users.noreply.github.com>
2020-03-24 14:02:07 +0100

    3/24/2020

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- Made by Gino -->
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="DC Heroes">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Gino's Portfolio</title>
+</head>
+<body>
+<div class="Container">
+	<header class="navigationIndex">
+		<nav>
+		<li>
+			<lu class="navigatorText">Home</lu>
+			<lu class="navigatorText">About</lu>
+			<lu class="navigatorText">Projects</lu>
+			<lu class="navigatorText">Contact</lu>
+		</li>
+		</nav>
+	</header>
+
+	<div class="mainContainer">
+		<img class="Slides" src="img/bgIndex.jpg"></img>
+		<div class="clickableButton"></div>
+	</div>
+
+
+</div>
+</body>
+</html>
+
